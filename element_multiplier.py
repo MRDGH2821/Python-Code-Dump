7d2fbf968640fd6e210e16e9062b0e103459ea2b
MRDGH2821 <ask.mrdgh2821@outlook.com>
2024-03-03 13:43:37 +0000

    refactor: rename function

--- element_multiplier.py
+++ element_multiplier.py
@@ -3,11 +3,11 @@ random_numbers = [randint(1, 50) for _ in range(10)]
 print(random_numbers)
 
 
-def listmul(g):
+def multiply_list_elements(g):
     prd = 1
     for d in g:
         prd = prd * d
     return prd
 
 
-print("Product of elements of list =", listmul(random_numbers))
+print("Product of elements of list =", multiply_list_elements(random_numbers))
