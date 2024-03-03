04fc39a0ac992b968eb3d4b651161d37a93b6d5a
MRDGH2821 <ask.mrdgh2821@outlook.com>
2024-03-03 14:12:26 +0000

    refactor: optimise logic

--- triangle_determiner.py
+++ triangle_determiner.py
@@ -1,14 +1,10 @@
 x = int(input("Enter side 1:"))
 y = int(input("Enter side 2:"))
 z = int(input("Enter side 3:"))
+
 if x == y and y == z and x == z:
-    print("Equilatral triangle !")
+    print("Equilateral triangle !")
+elif x != y and y != z and z != x:
+    print("Scalene triangle !")
 else:
-    if x == y:
-        print("Isoceles triangle !")
-    elif y == z:
-        print("Isoceles triangle !")
-    elif z == x:
-        print("Isoceles triangle !")
-    else:
-        print("Scalene triangle !")
+    print("Isosceles triangle !")
--- vowel_counter.py
+++ vowel_counter.py
@@ -1,8 +1,11 @@
-# Div-C Roll no.42
-# This program counts the number of vowels
+"""
+This program counts the number of vowels
+"""
 fg = str(input("Enter a string:"))
 ct = 0
-for x in fg:  # for loop iterating in sring
-    if x == 'a' or x == 'e' or x == 'i' or x == 'o' or x == 'u' or x == 'A' or x == 'E' or x == 'I' or x == 'O' or x == 'U':  # checking character equal to vowel or not
+# for loop iterating in string
+for x in fg:
+    # checking character equal to vowel or not
+    if x.lower() in 'aeiou':
         ct += 1  # storing the count of vowels
 print(ct)
