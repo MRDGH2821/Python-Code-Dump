cd103418e2122687ccea9a56ac334166826c7698
MRDGH2821 <ask.mrdgh2821@outlook.com>
2024-03-07 14:36:43 +0000

    refactor: rename variable

--- src/sort_by_last_digit.py
+++ src/sort_by_last_digit.py
@@ -1,5 +1,5 @@
-l = [5, 7, 22, 97, 54, 62, 577, 623, 973, 661]
-# l =[str(i)[::-1] for i in l].sort()
-# print (l.sort(key=lambda x:x%10,reverse=False))
-# print (list(map(lambda x:x%10,l)).sort())
-print([i for i in l if i == i % 10].sort())
+numbers = [5, 7, 22, 97, 54, 62, 577, 623, 973, 661]
+# numbers = [str(i)[::-1] for i in numbers].sort()
+# print(numbers.sort(key=lambda x: x % 10, reverse=False))
+# print(list(map(lambda x: x % 10, numbers)).sort())
+print([i for i in numbers if i == i % 10].sort())
