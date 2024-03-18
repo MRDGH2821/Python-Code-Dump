6cc028edf261721b239be229696e513b475580e2
MRDGH2821 <ask.mrdgh2821@outlook.com>
2024-03-18 16:40:03 +0000

    refactor: use enums

--- src/weekday_name.py
+++ src/weekday_name.py
@@ -1,17 +1,32 @@
-day = int(input("Enter no. of day:"))
-if (day == 1) or (day % 7 == 1):
-    print("Monday")
-elif (day == 2) or (day % 7 == 2):
-    print("Tuesday")
-elif (day == 3) or (day % 7 == 3):
-    print("Wednesday")
-elif (day == 4) or (day % 7 == 4):
-    print("Thursday")
-elif (day == 5) or (day % 7 == 5):
-    print("Friday")
-elif (day == 6) or (day % 7 == 6):
-    print("Saturday")
-elif (day == 7) or (day % 7 == 7):
-    print("Sunday")
-else:
-    print("Invalid Number")
+"""Weekday Name.
+
+Determines the name of given day number.
+"""
+
+from enum import Enum
+
+
+class Weekdays(Enum):
+    """Enum class for weekdays."""
+
+    Sunday = 0
+    Monday = 1
+    Tuesday = 2
+    Wednesday = 3
+    Thursday = 4
+    Friday = 5
+    Saturday = 6
+
+    @classmethod
+    def print_day(cls, date: int) -> str:
+        """Print the name of the day of the week."""
+        name = "Invalid Number"
+        for day in cls:
+            if date % 7 == day.value:
+                name = day.name
+                break
+        return name
+
+
+day = int(input("Enter day number: "))
+print(Weekdays.print_day(day))
