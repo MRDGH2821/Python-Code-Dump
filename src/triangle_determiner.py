4a9ad985769f92e17549d259714b8753e0792238
MRDGH2821 <ask.mrdgh2821@outlook.com>
2024-03-18 16:49:38 +0000

    refactor: change comparision logic & add docs

--- src/triangle_determiner.py
+++ src/triangle_determiner.py
@@ -1,10 +1,15 @@
+"""Triangle Determiner.
+
+Determines the type of Triangle based on the sides
+"""
+
 x = int(input("Enter side 1:"))
 y = int(input("Enter side 2:"))
 z = int(input("Enter side 3:"))
 
 if x == y and y == z and x == z:
     print("Equilateral triangle !")
-elif x != y and y != z and z != x:
+elif x not in (y, z) and y != z:
     print("Scalene triangle !")
 else:
     print("Isosceles triangle !")
