ab2112e085d2ff0ff0bfa694086a4a91f64d5e19
MRDGH2821 <ask.mrdgh2821@outlook.com>
2024-03-18 16:46:27 +0000

    refactor: rename variables & add docs

--- src/vowel_counter.py
+++ src/vowel_counter.py
@@ -1,12 +1,13 @@
-"""
-This program counts the number of vowels
+"""Vowel counter.
+
+This program counts the number of vowels.
 """
 
-fg = str(input("Enter a string:"))
-ct = 0
+string = str(input("Enter a string:"))
+count = 0
 # for loop iterating in string
-for x in fg:
+for x in string:
     # checking character equal to vowel or not
     if x.lower() in "aeiou":
-        ct += 1  # storing the count of vowels
-print(ct)
+        count += 1  # storing the count of vowels
+print(count)
