9afd7a43fb119af348665d34b94cbf65ce1aa48a
MRDGH2821 <ask.mrdgh2821@outlook.com>
2024-03-18 16:52:57 +0000

    refactor: rename variable & add docs

--- src/sum_of_digits.py
+++ src/sum_of_digits.py
@@ -1,6 +1,8 @@
-sum = 0
+"""Sum of digits."""
+
+digit_sum = 0
 a = int(input("Enter number to find sum of digits: "))
 while a > 0:
-    sum = sum + int(a % 10)
+    digit_sum = digit_sum + int(a % 10)
     a = a // 10
-print("Sum is", sum)
+print("Sum is", digit_sum)
