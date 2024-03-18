3a9b78fe5f8eaf4c7ddca7495e830b2673a1b6bc
MRDGH2821 <ask.mrdgh2821@outlook.com>
2024-03-18 16:45:09 +0000

    refactor: use different logic to check vowels

--- src/vowel_detailed_counter.py
+++ src/vowel_detailed_counter.py
@@ -1,16 +1,22 @@
-ste = str(input("Enter a string "))
-ct = 0
-for x in ste:
-    if x == "a" or "e" or "i" or "o" or "u" or "A" or "E" or "I" or "O" or "U":
-        ct = ct + 1
-print("Total vowels = ", ct)
-print("a= ", ste.count("a", 0, len(ste)))
-print("e= ", ste.count("e", 0, len(ste)))
-print("i= ", ste.count("i", 0, len(ste)))
-print("o= ", ste.count("o", 0, len(ste)))
-print("u= ", ste.count("u", 0, len(ste)))
-print("A= ", ste.count("A", 0, len(ste)))
-print("E= ", ste.count("E", 0, len(ste)))
-print("I= ", ste.count("I", 0, len(ste)))
-print("O= ", ste.count("O", 0, len(ste)))
-print("U= ", ste.count("U", 0, len(ste)))
+"""Detailed Vowels Counter.
+
+Counts vowels with their respective case.
+"""
+
+string = str(input("Enter a string:\n"))
+count = 0
+vowels = "aeiou"
+for letter in string:
+    if letter in vowels or letter in vowels.upper():
+        count = count + 1
+print("Total vowels = ", count)
+print("a = ", string.count("a", 0, len(string)))
+print("e = ", string.count("e", 0, len(string)))
+print("i = ", string.count("i", 0, len(string)))
+print("o = ", string.count("o", 0, len(string)))
+print("u = ", string.count("u", 0, len(string)))
+print("A = ", string.count("A", 0, len(string)))
+print("E = ", string.count("E", 0, len(string)))
+print("I = ", string.count("I", 0, len(string)))
+print("O = ", string.count("O", 0, len(string)))
+print("U = ", string.count("U", 0, len(string)))
