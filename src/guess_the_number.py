bf2cc6aa7052ce1f4bfa3c1f858844c282f56df4
MRDGH2821 <ask.mrdgh2821@outlook.com>
2024-04-16 10:44:55 +0000

    refactor: use secrets module for random number
    Rename vairables for clarity

--- src/guess_the_number.py
+++ src/guess_the_number.py
@@ -1,11 +1,12 @@
 """Guess the number."""
 
-from random import randint
+from secrets import randbelow
 
-x = randint(1, 10)
-a = 0
-b = 0
-while a != x:
-    a = int(input("Enter a no "))
-    b += 1
-print("The no is", x, ". Guessed in ", b, "tries")
+print("Guess the number!")
+secret_number = randbelow(10)
+guessed = 0
+tries = 0
+while guessed != secret_number:
+    guessed = int(input("Enter a no: "))
+    tries += 1
+print("The no is", secret_number, ". Guessed in", tries, "tries")
